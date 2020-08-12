15c73d405ce7a17aa541b8f9f1d65f102b31f35e
sergeykasyan <s.kasyan@list.ru>
2020-08-12 14:45:22 +0300

    Update links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Sobel filter visualization
 
 The Sobel operator, sometimes called the Sobel–Feldman operator or Sobel filter, is used in image processing and computer vision, particularly within edge detection algorithms where it creates an image emphasising edges.
+
+[Demo](https://sergey121.github.io/Sobel-filter-visualization/)
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ export default () => {
     <nav className="header">
       <div className="header__name">Sobel filter visualization</div>
       <div className="header__github">
-        <a className="header__link" target={'_blank'} rel="noopener noreferrer" href="https://github.com/Sergey121/nonogram-app/">View on GitHub</a>
+        <a className="header__link" target={'_blank'} rel="noopener noreferrer" href="https://github.com/Sergey121/Sobel-filter-visualization/">View on GitHub</a>
       </div>
     </nav>
   )
